2d37e06c19b900fa6322532aaa2c7be61ec025c3
inevitable360 <pvidalx@gmail.com>
2024-04-11 21:46:46 +0100

    Added test using node IP as argument
    Added test using node IP as argument to test a single node
    
    fixed the DNS query of the Dogecoin network using a seed

--- cmd/dogenet/main.go
+++ cmd/dogenet/main.go
@@ -1,7 +1,32 @@
 package main
 
-import dogenet "github.com/dogeorg/dogenet/pkg"
+import (
+	"fmt"
+	"net"
+	"os"
+
+	dogenet "github.com/dogeorg/dogenet/pkg"
+)
 
 func main() {
-	dogenet.TestNode("seed.multidoge.org")
+
+	// Check if an IP address is provided as a command line argument
+	if len(os.Args) > 1 {
+		// Test the Dogecoin node
+		dogenet.TestNode(os.Args[1])
+	} else {
+		// Query all Dogecoin network
+		seedNodes := []string{"seed.multidoge.org"}
+		for _, node := range seedNodes {
+			ips, err := net.LookupIP(node)
+			if err != nil {
+				fmt.Println("Error resolving DNS:", err)
+				continue
+			}
+			for _, ip := range ips {
+				dogenet.TestNode(ip.String())
+			}
+		}
+	}
+
 }
