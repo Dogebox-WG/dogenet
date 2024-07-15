fa5aafbe81d79d1bd9c119df8d71c08bf1685290
Raffe <andrew@dogecoin.com>
2024-07-15 14:51:35 +1000

    Clarified Doge Epoch Time

--- internal/gossip/protocol/time.go
+++ internal/gossip/protocol/time.go
@@ -3,19 +3,26 @@ package protocol
 import "time"
 
 const (
-	// Midnight, December 6, 2013 in UNIX Epoch Time
-	// Subtract this from UNIX Timestamp to get Doge Epoch Time
+	// DogeNet timestamps are 32-bit unsigned "Doge Epoch Time"
+	// which is UNIX Epoch Time starting from Dec 6, 2013.
+	// Range: 2013 - 2149
+
+	// Subtract this from a UNIX Timestamp to get Doge Epoch Time.
+	// Midnight, December 6, 2013 in UNIX Epoch Time.
 	DogeEpoch = 1386288000
+
+	// One Day in UNIX Epoch Time (and Doge Epoch)
+	OneDay = 86400
 )
 
 func DogeNow() uint32 {
 	return uint32(time.Now().Unix() - DogeEpoch)
 }
 
-func DogeToTime(ts uint32) time.Time {
+func DogeToUnix(ts uint32) time.Time {
 	return time.Unix(int64(ts)+DogeEpoch, 0)
 }
 
-func TimeToDoge(time time.Time) uint32 {
+func UnixToDoge(time time.Time) uint32 {
 	return uint32(time.Unix() - DogeEpoch)
 }
